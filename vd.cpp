bdfa7d2bdf074578f6047f40e17887135c269d47
TTMChung <95177130+TTMChung@users.noreply.github.com>
2021-12-01 16:46:55 +0700

    Add files via upload

---
+++ vd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char ch;
+    int i=0;
+    if (i<0) ch='C';
+    else if(i<=2) ch='A';
+    else if(i<=8) ch='B';
+    else ch='C';
+    cout<<ch;
+
+    return 0;
+}
